788de7e27ea39bc6c8706ada112be26d70d7b464
Marek Rzepka <marku782@gmail.com>
2019-04-14 19:59:04 +0200

    kosmetyka

---
+++ storage/framework/views/742e7b197cc3e60bf2fad15294a87ccc4b2c7c88.php
@@ -0,0 +1,5 @@
+<footer class="py-5">
+    <div class="container">
+        <?php echo $__env->make('layouts.footers.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    </div>
+</footer><?php /**PATH D:\Sem I i II MGR\Aplikacje Internetowe\xampp\htdocs\eFaktura\resources\views/layouts/footers/guest.blade.php ENDPATH**/ ?>
